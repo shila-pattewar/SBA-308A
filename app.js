8381ece584c2840f0fe77611983f8792bfb96a73
shila <shilapattewar16@gmail.com>
2025-01-12 16:56:16 -0600

    create post method in app.js

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ async function axiosTopHeadlines() {
   try {
     const response = await axios(url);
     const data = response.data;
+    console.log(response.data);
    // const data = await response.json();
     
     // Display the articles and description
@@ -23,8 +24,9 @@ async function axiosTopHeadlines() {
       articles.forEach(article => {
         articlesHTML += `
           <div>
+            <h4><strong>Author:</strong> ${article.author}</h4>
             <h3><a href="${article.url}" target="_blank">${article.title}</a></h3>
-            <p>${article.description}</p>
+            <p><strong>Description:</strong> ${article.description}</p>
           </div>
         `;
       });
@@ -35,6 +37,73 @@ async function axiosTopHeadlines() {
     console.error('Error fetching news:', error);
   }
 }
+
+function createPostDiv(post) {
+    const div = document.getElementById('posts');
+  
+    div.innerHTML = `
+      <h1>${post.author}</h1>
+      <p>${post.description}</p>
+      <p>${post.title}</p>
+    `;
+  }
+
+
+
+  axios.post('https://jsonplaceholder.typicode.com/posts', {
+    author: 'Test Author',
+    description: 'Test Description',
+    title: 'Test Title',
+  })
+  .then((res) => console.log(res.data));
+
+
+// Posting Data with Fetch
+const form = document.getElementById('createPost');
+const author = document.getElementById('author');
+const description = document.getElementById('description');
+const title = document.getElementById('title');
+
+form.addEventListener('submit', createPost);
+
+async function createPost(evt) {
+  evt.preventDefault();
+  const post = {
+    author: author.value,
+    description: description.value,
+    title: title.value,
+  };
+
+  try {
+    const OPTIONS = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(post),
+    };
+
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/posts',
+      OPTIONS
+    );
+    const data = await response.json();
+    createPostDiv(data);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function createPostDiv(post) {
+  const div = document.getElementById('posts');
+
+  div.innerHTML = `
+    <h1>${post.author}</h1>
+    <p>${post.description}</p>
+    <p>${post.title}</p>
+  `;
+}
+
 
 // Fetch top headlines on page load
 axiosTopHeadlines();
